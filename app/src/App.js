0ced00224524970411ca2b40e4431ead691de8ea
joaonunez <joaovaldiglesias@gmail.com>
2024-07-09 08:47:48 -0400

    avanzando

--- app/src/App.js
+++ app/src/App.js
@@ -4,19 +4,34 @@ import { FormularioContador } from './components/FormularioContador';
 
 const App = () => {
   const [seconds, setSeconds] = useState(0);
+  const [isRunning, setIsRunning] = useState(true);
 
   useEffect(() => {
+    if (!isRunning) return;
+
     const interval = setInterval(() => {
       setSeconds(prevSeconds => prevSeconds + 1);
     }, 1000);
 
     return () => clearInterval(interval);
-  }, []);
+  }, [isRunning]);
+
+  const handleStop = () => {
+    setIsRunning(false);
+  };
+  const handlePlay = () => {
+    setIsRunning(true);
+  };
+
+  const handleReset = () => {
+    setSeconds(0);
+    setIsRunning(true);
+  };
 
   return (
     <div>
       <ContadorSegundos seconds={seconds} />
-      <FormularioContador></FormularioContador>
+      <FormularioContador onStop={handleStop} onReset={handleReset} onPlay={handlePlay} />
     </div>
   );
 };
--- app/src/components/FormularioContador.jsx
+++ app/src/components/FormularioContador.jsx
@@ -1,20 +1,20 @@
 import React from "react";
-export function FormularioContador() {
+
+export function FormularioContador({ onStop, onReset, onPlay }) {
   return (
     <>
-    <div className="mb-5 col-xxl-2 m-auto d-flex flex-row justify-content-around">
-        <button className="btn btn-danger">Detener</button>
-        <button className="btn btn-warning">Reiniciar</button>
-
+      <div className="mb-5 col-xxl-2 m-auto d-flex flex-row justify-content-around">
+        <button className="btn btn-danger" onClick={onStop}>Detener</button>
+        <button className="btn btn-success" onClick={onPlay}>Continuar</button>
+        <button className="btn btn-warning" onClick={onReset}>Reiniciar</button>
       </div>
-      <div className="col-xxl-2 m-auto">
+      {/* <div className="col-xxl-2 m-auto">
         <input
           className="m-auto text-center d-flex justify-content-center form-control"
           type="text"
           placeholder={"Ingresa valor"}
         />
-      </div>
-      
+      </div> */}
     </>
   );
 }
